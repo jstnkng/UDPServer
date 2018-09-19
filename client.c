0f799a584953f4fb9b0c9f44babc6ecd56071261
Justin King <jstndvdkng@gmail.com>
2018-09-19 10:14:05 -0400

    Add UDP Server and client

---
+++ client.c
@@ -0,0 +1,48 @@
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+
+//Justin King
+//CIS 457
+//Client.c
+int main(int argc, char** argv)
+{
+	printf("Enter server IP Address: ");
+	char serverIP[20];
+	fgets(serverIP, 20, stdin);
+	printf("Enter port number: ");
+	char port[10];
+	fgets(port, 10, stdin);
+	int portNumber;
+	portNumber = atoi(port);
+
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0)
+	{
+		printf("Error creating socket");
+		return 1;
+	}
+	
+	struct sockaddr_in serveraddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(portNumber);
+	serveraddr.sin_addr.s_addr = inet_addr(serverIP);
+
+	printf("Enter a message: ");
+	char line[5000];
+	fgets(line, 5000, stdin);
+	sendto(sockfd, line, strlen(line) + 1, 0, (struct sockaddr*) &serveraddr, sizeof(serveraddr));
+
+	int len = sizeof(&serveraddr);
+	recvfrom(sockfd, line, 5000, 0, (struct sockaddr*) &serveraddr, &len);
+
+	printf("Received from server: %s\n", line);
+
+	close(sockfd);
+
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,39 @@
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+
+
+//Justin King
+//CIS 457
+//Server.c
+int main(int argc, char** argv)
+{
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	printf("Enter port number: ");
+	int port;
+	scanf("%d", &port);
+
+	struct sockaddr_in serveraddr, clientaddr;
+
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(port);
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
+
+	bind(sockfd, (struct sockaddr*) &serveraddr, sizeof(serveraddr));
+		
+	while(1)
+	{
+		int len = sizeof(clientaddr);
+		char line[5000];
+		recvfrom(sockfd, line, 5000, 0, (struct sockaddr*) &clientaddr, &len);
+		sendto(sockfd, line, strlen(line),0,
+			       (struct sockaddr*) &clientaddr, len);
+		
+		printf("Received from client: %s\n", line);
+	}
+	return 0;
+}
